e1003c85120646715ad92fdd12eb426cb0dc96a0
Maksym Kostiv <rafik9202@gmail.com>
2025-03-24 22:53:57 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ export const App: React.FC = () => {
         <button
           type="button"
           className={classNames('button', 'is-info', {
-            'is-light': sortField !== SortType.alphabetically,
+            'is-light': sortField === SortType.alphabetically,
           })}
           onClick={() => setSortField(SortType.alphabetically)}
         >
@@ -84,7 +84,7 @@ export const App: React.FC = () => {
         <button
           type="button"
           className={classNames('button', 'is-success', {
-            'is-light': sortField !== SortType.length,
+            'is-light': sortField === SortType.length,
           })}
           onClick={() => setSortField(SortType.length)}
         >
@@ -94,7 +94,7 @@ export const App: React.FC = () => {
         <button
           type="button"
           className={classNames('button', 'is-warning', {
-            'is-light': !reversed,
+            'is-light': reversed,
           })}
           onClick={() => setReversed(!reversed)}
         >
